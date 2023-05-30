1cb139eb68adfb22cc0ab4a9c75d090b4502d7c5
Bepul-Hossain <bepul.cse.ru@gmail.com>
2023-05-30 17:36:18 +0600

    task 2

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 
 const Problem2 = () => {
+    const [show, setShow] = useState(false);
+    const [data, setData] = useState([]);
+    const handleModal = () => {
+        setShow(!show);
+    }
+   
+
+    useEffect(() => {
+        fetch('https://contact.mediusware.com/api/contacts/',)
+           .then((response) => response.json())
+           .then((res) => {
+              console.log(res);
+                setData(res);
+           })
+           .catch((err) => {
+              console.log(err);
+           });
+     }, []);
 
     return (
 
         <div className="container">
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
+
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button className="btn btn-lg btn-outline-primary" type="button" onClick={handleModal}>All Contacts</button>
+                    <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
                 </div>
-                
+
+                {
+                    show && (
+                        <div style={{height:'80vh', width:'80vw', top:'30px', position:'fixed', background:'gray'}}>
+                            <h3 >Modal A</h3>
+                            <section style={{display:'flex', justifyContent:'center'}}>
+                                {/* button A=> switch to modal B */}
+                                <button style={{backgroundColor:'#46139f'}}>All Contacts</button>
+                                {/* button B=> switch to modal A */}
+                                <button style={{backgroundColor:'#ff7f50'}}>Contact US</button>
+                                {/* button C */}
+                                <button style={{backgroundColor:'white', borderColor:'#46139f'}} type="button" onClick={handleModal}>
+                                    Close
+                                </button>
+                            </section>
+                        </div>
+                    )
+                }
             </div>
         </div>
     );
